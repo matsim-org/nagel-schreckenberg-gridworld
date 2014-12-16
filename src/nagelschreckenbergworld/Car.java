bbdd4cd9afdeb9c3cc4d50c7edbf6706a4d6c95e
Michael Zilske <michael.zilske@tu-berlin.de>
2014-12-16 19:10:12 +0100

    comments

--- src/nagelschreckenbergworld/Car.java
+++ src/nagelschreckenbergworld/Car.java
@@ -6,9 +6,13 @@ import info.gridworld.grid.Location;
 
 class Car extends Actor {
 
+    // These are values which are "too bad" for reality, but reliably produce
+    // serious congestion, so that there is something to see.
     int maxSpeed = 3;
     double slowdownRate = 0.5;
 
+    // Initial speed is 0 - We put in new cars from the left-hand side of the screen,
+    // and they are initially standing. This could be the end of a bottleneck.
     int speed = 0;
 
     public Car() {
@@ -33,10 +37,12 @@ class Car extends Actor {
         Location next = getLocation();
         int i = 0;
         while (i < speed) {
+            // Peek to the right until I see a car in front of me, up to my maximum speed.
             next = next.getAdjacentLocation(getDirection());
             if (grid.isValid(next)) {
                 Actor occupant = grid.get(next);
                 if (occupant != null) break;
+                // This place is occupied, so this is my new maximum speed.
             }
             i++;
         }
@@ -53,11 +59,13 @@ class Car extends Actor {
         Grid<Actor> grid = getGrid();
         Location next = getLocation();
         for (int j = 0; j < speed; j++) {
+            // Walk speed places to the right.
             next = next.getAdjacentLocation(getDirection());
         }
         if (grid.isValid(next)) {
             moveTo(next);
         } else {
+            // Drive out of the right-hand side of the screen.
             removeSelfFromGrid();
         }
     }
--- src/nagelschreckenbergworld/CarRunner.java
+++ src/nagelschreckenbergworld/CarRunner.java
@@ -9,9 +9,10 @@ public class CarRunner {
 
     public static void main(String[] args) {
         ActorWorld world = new ActorWorld();
-        int nLanes=1;
-        world.setGrid(new BoundedGrid<Actor>(nLanes,200));
-        for (int j=0; j<nLanes; j++) {
+        int nLanes = 1;
+        int nPlaces = 200;
+        world.setGrid(new BoundedGrid<Actor>(nLanes, nPlaces));
+        for (int j = 0; j < nLanes; j++) {
             world.add(new Location(j,0), new CarSource(world));
         }
         world.show();
